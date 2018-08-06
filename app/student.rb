21973a41201656d53d5757bfdf3eb46195ae1577
Chuck Thompson <chuckthompson27@me.com>
2018-08-05 20:53:10 -0700

    Basic functionality model. Begin testing.

--- app/student.rb
+++ app/student.rb
@@ -5,21 +5,38 @@ class School
     @roster = { }
   end
 
+  # Add student and grade level to student roster
   def add(student, grade)
-    roster[grade] = []
-    roster[grade] << student
-  end
-
-  def all
-    roster.each do |key , value|
-      puts "#{value} is in grade #{key}"
+    # If the grade doesn't exist then initialize it
+    if roster[grade] == nil
+      roster[grade] = []
+      roster[grade] << student
+      # Otherwise push student onto key's array of vals
+    else
+      roster[grade] << student
     end
   end
 
-  def grade(grade)
-    puts roster[grade]
+  # For each entry in the roster, list it's grade and students
+  def all
+    roster.each do |key , value|
+       puts "#{value} is in grade #{key}"
+    end
   end
 
+  # Return all students in the given grade
+  def grade(grade)
+    roster.each do |k, v|
+      if k == grade
+        return v
+      end
+    end
+  end
+
+  def sort
+    # sorted_roster = roster.sort_by {|k, v| k}
+    # puts sorted_roster
+  end
 
 end
 
