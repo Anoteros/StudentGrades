5ceab338458b1830609458510bbcf5dbb4c4a41b
Chuck Thompson <chuckthompson27@me.com>
2018-08-05 20:49:20 -0700

    Added testing. Added testing setup. Refactored student.rb

--- test/student_spec.rb
+++ test/student_spec.rb
@@ -3,8 +3,22 @@ require_relative './../app/student'
 
 describe 'student class' do
 
+    before :each do
+      @school = School.new
+      @school.add("Bob", 12)
+      @school.add("Chris", 12)
+      @school.add("Steve", 10)
+    end
+
     it "initializes school class" do
-      x = School.new
-      expect(x.class) == School
+      expect(@school.class) == School
+    end
+
+    it "should add students to roster" do
+      expect @school.all == !nil
+    end
+
+    it "should list all students in given grade" do
+      expect(@school.grade(10)).to eq(["Steve"])
     end
 end
